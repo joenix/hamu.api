977a60514d89803c2e33d1da278681b87ac65f4c
Joenix <joenix.com@gmail.com>
2024-07-19 19:53:34 +0800

    feat: update

--- server/app.js
+++ server/app.js
@@ -8,10 +8,10 @@ const router = new Router();
 
 const APPID = `wx5d458ff8b11233f0`;
 const APPSECRET = `d4b3ef98adbf73cf3d3faffcaab52b21`;
-const REDIRECT_URI = `http://localhost:7086/callback`;
+const REDIRECT_URI = `http://127.0.0.1:7086/callback`;
 
 // 微信验证接口
-router.get('/wechat', async (ctx) => {
+router.get('/', async (ctx) => {
   const { signature, timestamp, nonce, echostr } = ctx.query;
   const token = `hamuai`;
 
